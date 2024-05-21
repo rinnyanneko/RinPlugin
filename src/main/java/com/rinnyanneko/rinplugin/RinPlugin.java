a283721d85fd24fe42c36e905c2ffa048aebbb0d
凛喵 <rinnyanneko@outlook.com>
2024-05-21 23:49:26 +0800

    a

--- src/main/java/com/rinnyanneko/rinplugin/RinPlugin.java
+++ src/main/java/com/rinnyanneko/rinplugin/RinPlugin.java
@@ -1,6 +1,6 @@
 package com.rinnyanneko.rinplugin;
 
-import com.rinnyanneko.rinplugin.commands.about;
+import com.rinnyanneko.rinplugin.commands.About;
 import com.rinnyanneko.rinplugin.events.PlayerJoin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,7 +10,7 @@ public final class RinPlugin extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         this.getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
-        this.getCommand("about").setExecutor(new about());
+        this.getCommand("about");//.setExecutor(new About());
     }
 
     @Override
--- src/main/java/com/rinnyanneko/rinplugin/commands/About.java
+++ src/main/java/com/rinnyanneko/rinplugin/commands/About.java
@@ -5,7 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-public class about implements CommandExecutor {
+public class About implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
             if (label.equalsIgnoreCase("about")) {
