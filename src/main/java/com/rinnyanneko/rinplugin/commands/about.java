9722130b1247451081ebc73b408794be9fe3c670
rinnyanneko <rinnyanneko@outlook.com>
2024-05-21 22:26:45 +0800

    a

--- src/main/java/com/rinnyanneko/rinplugin/commands/about.java
+++ src/main/java/com/rinnyanneko/rinplugin/commands/about.java
@@ -8,12 +8,18 @@ import org.bukkit.command.CommandSender;
 public class about implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(label.equalsIgnoreCase("about")){
-            sender.sendMessage(ChatColor.AQUA + "[About]");
-            sender.sendMessage(ChatColor.AQUA + "Rin's Plugin");
-            sender.sendMessage(ChatColor.AQUA + "This server is running RinPlugin v1.0.0");
-            sender.sendMessage(ChatColor.AQUA + "Developed by Rinnyanneko");
-            return true;
+        try {
+            if (label.equalsIgnoreCase("about")) {
+                sender.sendMessage(ChatColor.AQUA + "[About]");
+                sender.sendMessage(ChatColor.AQUA + "Rin's Plugin");
+                sender.sendMessage(ChatColor.AQUA + "This server is running RinPlugin v1.0.0");
+                sender.sendMessage(ChatColor.AQUA + "Developed by Rinnyanneko");
+                return true;
+            }
+        } catch (Exception e) {
+            sender.sendMessage(ChatColor.RED + "An error occurred while executing this command.%n" + String.valueOf(e));
+            e.printStackTrace();
+            return false;
         }
         return false;
     }
