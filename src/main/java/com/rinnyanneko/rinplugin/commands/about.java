5d05df0380713d24fc906b5103dd1094bf99b5f7
凛喵 <rinnyanneko@outlook.com>
2024-05-21 23:46:03 +0800

    new branch

--- src/main/java/com/rinnyanneko/rinplugin/commands/about.java
+++ src/main/java/com/rinnyanneko/rinplugin/commands/about.java
@@ -8,19 +8,13 @@ import org.bukkit.command.CommandSender;
 public class about implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        try {
             if (label.equalsIgnoreCase("about")) {
                 sender.sendMessage(ChatColor.AQUA + "[About]");
                 sender.sendMessage(ChatColor.AQUA + "Rin's Plugin");
                 sender.sendMessage(ChatColor.AQUA + "This server is running RinPlugin v1.0.0");
-                sender.sendMessage(ChatColor.AQUA + "Developed by Rinnyanneko");
+                sender.sendMessage(ChatColor.AQUA + "Developed by rinnyanneko");
                 return true;
             }
-        } catch (Exception e) {
-            sender.sendMessage(ChatColor.RED + "An error occurred while executing this command.%n" + String.valueOf(e));
-            e.printStackTrace();
-            return false;
-        }
         return false;
     }
 }
